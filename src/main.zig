9488eba8a1da882b69e013eb4b2d7cd5cdc280b6
elviscgn <96030189+elviscgn@users.noreply.github.com>
2025-11-09 04:26:47 +0200

    initial commit and setup raylib

---
+++ src/main.zig
@@ -0,0 +1,30 @@
+const rl = @import("raylib");
+
+pub fn main() anyerror!void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "Chess");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    // Main game loop
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+
+        rl.drawText("Hallo warudooo!", 190, 200, 20, .light_gray);
+    }
+}
